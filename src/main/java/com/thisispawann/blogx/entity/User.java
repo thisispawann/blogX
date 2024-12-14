28a9ba49a99d8ea1781d04e8e076fd7c6b29490b
Pawan Kumar Shrestha <thisispawann@gmail.com>
2024-12-14 23:24:22 +0545

    :: tested with postgres ::

--- src/main/java/com/thisispawann/blogx/entity/User.java
+++ src/main/java/com/thisispawann/blogx/entity/User.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name="username", nullable = false, length = 100, unique=true)
